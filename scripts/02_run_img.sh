37bb00341fcd01c2fae33c12566ff1a01519db87
Fabricio Correa <faerroc@gmail.com>
2025-12-07 22:40:34 -0500

    Update to use vncviewer

--- scripts/02_run_img.sh
+++ scripts/02_run_img.sh
@@ -2,6 +2,6 @@
 # Run this command outside the container
 #
 
-docker run -ti -v /home/fcorrea/src/zephyrproject:/workdir \
-			   -v $PWD:/workdir/desenvolvimento            \
+docker run -ti -p 5900:5900 -v /home/fcorrea/src/zephyrproject:/workdir \
+			   -v $PWD:/workdir/desenvolvimento                         \
                 zephyr-build:v100 /bin/bash
--- scripts/03_update_img.sh
+++ scripts/03_update_img.sh
@@ -5,7 +5,7 @@
 cd /workdir
 rm -rf .west/
 sync
-west init
+west init -m https://github.com/zephyrproject-rtos/zephyr --mr v4.3.0
 west update
 west build -b qemu_x86 samples/hello_world
 west build -t run
